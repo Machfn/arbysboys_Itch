5d457d91e5d95b19f57068fca66e2d2461cc2029
Pablo <Pablo>
2026-02-20 16:05:39 -0700

    added conversion

--- src/conversion.py
+++ src/conversion.py
@@ -1,5 +1,38 @@
+import json 
+
+class Json():
+    def __init__(self,json_filename,c_file_txt):
+        self.json_filename = json_filename
+        self.c_file_txt = c_file_txt
+
+    def open_file(self):
+        with open(self.json_filename, "r") as f:
+            data = json.load(f)
+
+        return data
+
+    def empty_file(self):
+        with open(c_file_txt,"w") as e:
+            e.write("")
+
+    def write_file(self,file_contents):
+        with open(self.c_file_txt, "a") as c:
+            for j in file_contents:
+                c.write(j)
 
 
-class Conversion:
-    def __init__(self):
-        self.started = True
+    def parse_arr(self,data):
+        file_contents = []
+
+        for i in data:
+            type = i["type"]
+
+            if (type == "pin_mode"):
+
+            else if ("pin_write"):
+
+            else if ("set_variable"):
+
+            else if ("loop"):
+
+            else:
