5d973498527e802806eda1cbd406cf456e0d141a
Brayden W <williamsonmbrayden@gmail.com>
2026-02-20 17:14:00 -0700

    90% done conversion

--- src/conversion.py
+++ src/conversion.py
@@ -1,38 +1,61 @@
-import json 
+import json
+
 
 class Json():
-    def __init__(self,json_filename,c_file_txt):
-        self.json_filename = json_filename
-        self.c_file_txt = c_file_txt
+   def __init__(self,json_filename,c_file_txt):
+       self.json_filename = json_filename
+       self.c_file_txt = c_file_txt
 
-    def open_file(self):
-        with open(self.json_filename, "r") as f:
-            data = json.load(f)
+   def open_file(self):
+       with open(self.json_filename, "r") as f:
+           data = json.load(f)
+       return data
 
-        return data
+   def empty_file(self):
+       with open(self.c_file_txt,"w") as e:
+           e.write("")
 
-    def empty_file(self):
-        with open(c_file_txt,"w") as e:
-            e.write("")
+   def write_file(self,file_contents):
+       with open(self.c_file_txt, "a") as c:
+           for j in file_contents:
+               c.write(str(j))
 
-    def write_file(self,file_contents):
-        with open(self.c_file_txt, "a") as c:
-            for j in file_contents:
-                c.write(j)
+   def parse_arr(self,data):
+       file_contents = []
+
+       for i in data:
+           type = i["type"]
+
+           if (type == "pin_mode"):
+               file_contents.append(type + " " + i["pin"] + " " + i["mode"] + "\n")
+           elif (type == "pin_write"):
+               file_contents.append(type + " " + i["pin"] + " " + i["value"] + "\n")
+           elif (type == "set_variable"):
+               file_contents.append(type + " " + i["name"] + " " + i["value"] + "\n")
+           elif (type == "loop"):
+               file_contents.append(type + " " + i["times"] + " ")
+               file_contents.append(i["children"])
+               file_contents.append("\n")
+           else:
+               file_contents.append(type + " " + i["condition"] + " ")
+               file_contents.append(i["then"])
+               file_contents.append(" ")
+               file_contents.append(i["else"])
+               file_contents.append("\n")
+              
+       return file_contents
 
 
-    def parse_arr(self,data):
-        file_contents = []
 
-        for i in data:
-            type = i["type"]
 
-            if (type == "pin_mode"):
 
-            else if ("pin_write"):
 
-            else if ("set_variable"):
+j = Json("test.json","c.txt")
+data = j.open_file()
+file_contents = j.parse_arr(data)
+j.empty_file()
+j.write_file(file_contents)
 
-            else if ("loop"):
 
-            else:
+
+
